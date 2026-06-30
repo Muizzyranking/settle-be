3ba592721d587cfdf4f5c570497749fe0542b72a
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2026-06-30 14:13:13 +0100

    feat: add base model with id and created at as default fields

--- app/db/database.py
+++ app/db/database.py
@@ -1,6 +1,11 @@
+import uuid
 from collections.abc import AsyncGenerator
+from datetime import datetime, timezone
+
+from sqlalchemy import DateTime
+from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 from app.core.config import settings
 
@@ -30,7 +35,7 @@ async def init_db():
         await conn.run_sync(Base.metadata.create_all)
 
 
-async def get_db() -> AsyncGenerator[AsyncSession, None]:
+async def get_db() -> AsyncGenerator[AsyncSession]:
     async with AsyncSessionLocal() as session:
         try:
             yield session
@@ -40,3 +45,14 @@ async def get_db() -> AsyncGenerator[AsyncSession, None]:
             raise
         finally:
             await session.close()
+
+
+class BaseModel(Base):
+    __abstract__ = True
+
+    id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True), primary_key=True, default=uuid.uuid4
+    )
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), default=lambda: datetime.now(timezone.utc)
+    )
