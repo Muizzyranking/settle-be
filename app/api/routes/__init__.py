e6d2e77d36dcb43e5726759502b065ca20462fd1
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2026-06-30 19:49:25 +0100

    feat: include all routers

--- app/api/routes/__init__.py
+++ app/api/routes/__init__.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter
 
-from app.api.routes import webhook
+from app.api.routes import accounts, auth, collections, webhook
 
 router = APIRouter()
+router.include_router(auth.router, prefix="/auth", tags=["auth"])
+router.include_router(accounts.router, prefix="/accounts", tags=["accounts"])
+router.include_router(collections.router, prefix="/collections", tags=["collections"])
 router.include_router(webhook.router)
