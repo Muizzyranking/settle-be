613cce69ff95e8924f1e20b639d740950b32ed9c
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2026-06-30 16:39:54 +0100

    chore: allow db url

--- app/core/config.py
+++ app/core/config.py
@@ -17,14 +17,23 @@ class Settings(BaseSettings):
     ALLOWED_ORIGINS: list[str] = ["http://localhost:3000"]
 
     # Database
-    DB_HOST: str
-    DB_PORT: int = 5432
-    DB_USER: str
-    DB_PASSWORD: str
-    DB_NAME: str
+    DB_HOST: str | None = None
+    DB_PORT: int | None = 5432
+    DB_USER: str | None
+    DB_PASSWORD: str | None
+    DB_NAME: str | None
+    RAW_DATABASE_URL: str | None = None
 
     @property
     def DATABASE_URL(self) -> str:
+        if self.RAW_DATABASE_URL:
+            return self.RAW_DATABASE_URL.replace(
+                "postgresql://", "postgresql+asyncpg://"
+            )
+        if not all(
+            [self.DB_HOST, self.DB_PORT, self.DB_USER, self.DB_PASSWORD, self.DB_NAME]
+        ):
+            raise ValueError("Database configuration is incomplete.")
         return (
             f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}"
             f"@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
@@ -34,9 +43,12 @@ class Settings(BaseSettings):
     REDIS_HOST: str = "localhost"
     REDIS_PORT: int = 6379
     REDIS_PASSWORD: str | None = None
+    RAW_REDIS_URL: str | None = None
 
     @property
     def REDIS_URL(self) -> str:
+        if self.RAW_REDIS_URL:
+            return self.RAW_REDIS_URL
         if self.REDIS_PASSWORD:
             return (
                 f"redis://:{self.REDIS_PASSWORD}@{self.REDIS_HOST}:{self.REDIS_PORT}/0"
