468a10a5d33c1167c2b973fb1289565a221758e3
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2026-07-02 06:42:30 +0100

    feat: wire up all endpoints

--- app/api/routes/__init__.py
+++ app/api/routes/__init__.py
@@ -1,9 +1,13 @@
 from fastapi import APIRouter
 
-from app.api.routes import accounts, auth, collections, webhook
+from app.api.routes import accounts, auth, collections, pay, transactions, webhook
 
 router = APIRouter()
 router.include_router(auth.router, prefix="/auth", tags=["auth"])
 router.include_router(accounts.router, prefix="/accounts", tags=["accounts"])
 router.include_router(collections.router, prefix="/collections", tags=["collections"])
+router.include_router(pay.router, prefix="/pay", tags=["payment"])
+router.include_router(
+    transactions.router, prefix="/transactions", tags=["transactions"]
+)
 router.include_router(webhook.router)
