8dff97c5a4df90e17489daef4ced2b23f9cce4c6
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2026-07-04 15:45:14 +0100

    feat: wire up all route

--- app/api/routes/__init__.py
+++ app/api/routes/__init__.py
@@ -1,12 +1,31 @@
 from fastapi import APIRouter
 
-from app.api.routes import accounts, auth, collections, pay, transactions, webhook
+from app.api.routes import (
+    accounts,
+    auth,
+    collections,
+    dashboard,
+    finance,
+    notifications,
+    pay,
+    reports,
+    settings,
+    transactions,
+    webhook,
+)
 
 router = APIRouter()
 router.include_router(auth.router, prefix="/auth", tags=["auth"])
 router.include_router(accounts.router, prefix="/accounts", tags=["accounts"])
 router.include_router(collections.router, prefix="/collections", tags=["collections"])
+router.include_router(dashboard.router, prefix="/dashboard", tags=["dashboard"])
+router.include_router(finance.router, prefix="/finance", tags=["finance"])
+router.include_router(
+    notifications.router, prefix="/notifications", tags=["notifications"]
+)
 router.include_router(pay.router, prefix="/pay", tags=["payment"])
+router.include_router(reports.router, prefix="/reports", tags=["reports"])
+router.include_router(settings.router, prefix="/settings", tags=["settings"])
 router.include_router(
     transactions.router, prefix="/transactions", tags=["transactions"]
 )
